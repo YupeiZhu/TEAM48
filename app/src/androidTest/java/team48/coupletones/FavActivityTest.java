43dc32441d4f70c941d02f403086a8b2168608eb
Zhuyupei <yuz310@ucsd.edu>
2016-05-08 03:04:47 -0700

    11

---
+++ app/src/androidTest/java/team48/coupletones/FavActivityTest.java
@@ -0,0 +1,16 @@
+package team48.coupletones;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Zhuyupei on 5/8/16.
+ */
+public class FavActivityTest {
+
+    @Test
+    public void testReadFromFile() throws Exception {
+        assertTrue("dsd",true);
+    }
+}
